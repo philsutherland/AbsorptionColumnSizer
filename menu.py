8564a892f3f26dfc60769f55f37a6075fd4e8c04
Phil Sutherland <psuth026@uottawa.ca>
2019-07-05 16:57:22 -0400

    Driver file finished, main_menu finished

---
+++ menu.py
@@ -0,0 +1,34 @@
+class Menu():
+    @staticmethod
+    def main_menu():
+        selection = 0
+
+        print("""
+|-------------------------------------------------------------|
+|                          Main Menu                          |
+|-------------------------------------------------------------|
+| Options:                                                    |
+|        1. Size Absorption Column Menu                       |
+|        2. Optimize Absorption Column Menu                   |
+|        3. About                                             |
+|        4. Exit                                              |
+|-------------------------------------------------------------|
+        """)
+
+        try:
+            selection = int(input("Select Option: "))
+        except(BaseException):
+            print("Invalid Selection!")
+
+        if selection == 1:
+            sizeAbsorptionSubMenu()
+        elif selection == 2:
+            optimizeAbsorptionSubMenu()
+        elif selection == 3:
+            aboutSubMenu()
+        elif selection == 4:
+            print("Goodbye!")
+            sys.exit(0)
+        else:
+            print("Invalid Selection!")
+            mainMenu()
